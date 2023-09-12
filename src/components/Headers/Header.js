b618990a745f503e0b0e6ce9138e2d3482c02b77
WestOneTin <dnjstjr0507@gmailc.om>
2023-09-12 21:29:05 +0900

    update

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
         <Container fluid>
           <div className="header-body">
-            {/* Card stats */}
+            {/* Card stats
             <Row>
               <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
@@ -141,7 +141,7 @@ const Header = () => {
                   </CardBody>
                 </Card>
               </Col>
-            </Row>
+            </Row> */}
           </div>
         </Container>
       </div>
